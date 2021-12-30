5b8c14a0fd5704d91e13ab8f0b020f508ef053d1
Bohdan Leonets <leonetsbogdan@gmail.com>
2021-12-30 16:00:23 +0200

    log in and sign up forms were added

--- client/src/pages/AuthPage/AuthPage.jsx
+++ client/src/pages/AuthPage/AuthPage.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 import "../AuthPage/AuthPage.sass";
 
 const Authpage = () => {
   return (
     <>
       <div className="container">
-        <div className="auth-page flex f-column f-center">
-          <h3 className="has-text-centered is-size-1-tablet mb-6">
+        <div className="auth-page flex f-column f-center mt-6">
+          <h3 className="has-text-centered is-size-1-tablet mb-5">
             Log in Page
           </h3>
 
@@ -31,7 +31,7 @@ const Authpage = () => {
               </p>
             </div>
 
-            <div className="field mb-4">
+            <div className="field mb-5">
               <p className="control has-icons-left">
                 <input
                   className="input is-rounded"
@@ -47,11 +47,59 @@ const Authpage = () => {
 
             <div className="field">
               <p className="control">
-                <button className="button is-success is-rounded boxed bold-500">
+                <button className="button is-success is-rounded boxed bold-500 mb-2">
                   Log in
                 </button>
 
-                {/* <button className="button is-success is-rounded boxed bold-500">Sign up</button> */}
+                <a className="has-text-dark boxed" href="/">
+                  No account?
+                </a>
+              </p>
+            </div>
+          </form>
+
+          <form className="has-text-centered w400">
+            <div className="field">
+              <p className="control has-icons-left has-icons-right">
+                <input
+                  className="input is-rounded"
+                  type="email"
+                  placeholder="Email"
+                />
+
+                <span className="icon is-small is-left">
+                  <span className="mdi mdi-email"></span>
+                </span>
+
+                <span className="icon is-small is-right">
+                  <span className="mdi mdi-check-bold"></span>
+                </span>
+              </p>
+            </div>
+
+            <div className="field mb-5">
+              <p className="control has-icons-left">
+                <input
+                  className="input is-rounded"
+                  type="password"
+                  placeholder="Password"
+                />
+
+                <span className="icon is-small is-left">
+                  <span className="mdi mdi-lock"></span>
+                </span>
+              </p>
+            </div>
+
+            <div className="field">
+              <p className="control">
+                <button className="button is-success is-rounded boxed bold-500 mb-2">
+                  Sign up
+                </button>
+
+                <a className="has-text-dark boxed" href="/">
+                  Do you have an account?
+                </a>
               </p>
             </div>
           </form>
@@ -59,6 +107,6 @@ const Authpage = () => {
       </div>
     </>
   );
-}
+};
 
 export default Authpage;
