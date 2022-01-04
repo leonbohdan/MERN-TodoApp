4154148a9812a3de65715df077ad3ca86f4596a1
Bohdan Leonets <leonetsbogdan@gmail.com>
2022-01-04 16:39:05 +0200

    validation to email and password were added

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,27 +1,48 @@
-const { Router } = require('express');
-const User = require('../models/User');
+const { Router } = require("express");
+const User = require("../models/User");
 const router = Router();
+const { check, validationResult } = require("express-validator");
 
-router.post('/registration', async (req, res) => {
-  try {
-    const { email, password } = req.body;
+router.post(
+  "/registration",
+  [
+    check("email", "Email is not valid!").isEmail(),
+    check("password", "Password is not valid!").isLength({ min: 4 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
 
-    const isUsed = await User.findOne({ email });
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Not valid registration data!'
+        });
+      }
 
-    if (isUsed) {
-      return res.status(300).json({ message: 'This email is using by other person, you should use other one.' });
+      const { email, password } = req.body;
+
+      const isUsed = await User.findOne({ email });
+
+      if (isUsed) {
+        return res.status(300).json({
+          message:
+            "This email is using by other person, you should use other one.",
+        });
+      }
+
+      const user = new User({
+        email,
+        password,
+      });
+
+      await user.save();
+
+      res.status(201).json({ message: "New user was created!" });
+    } catch (error) {
+      console.error(error);
     }
-
-    const user = new User({
-      email, password
-    });
-
-    await user.save();
-
-    res.status(201).json({ message: 'New user was created!' });
-  } catch (error) {
-    console.error(error);
-  }
-});
+  },
+);
 
 module.exports = router;
